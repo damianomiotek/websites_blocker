8a0fccba3c1a31cc1452303b457925a940158df0
W118819 <damian.omiotek.external@worldline.com>
2024-02-28 23:32:08 +0100

    Finished implementation.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 This is Python application to block websites.
 
 To install open command line and paste: 
-pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ alarm-clock==1.5
-To run type in command line:
+pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ websites-blocker==2
+To run open command line as Administrator and paste:
 python -m websites_blocker
--- websites_blocker/__main__.py
+++ websites_blocker/__main__.py
@@ -1,15 +1,11 @@
-import ctypes, sys
+import sys
 from PyQt6.QtWidgets import QApplication
 from websites_blocker.main_window import MainWindow
 
 
-if ctypes.windll.shell32.IsUserAnAdmin():
-    app = QApplication(sys.argv)
+app = QApplication(sys.argv)
 
-    window = MainWindow()
-    window.show()
+window = MainWindow()
+window.show()
 
-    app.exec()
-else:
-    # Re-run the program with admin rights
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+app.exec()
